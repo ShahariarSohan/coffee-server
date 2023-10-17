a5613333e8825a70be6f2b0dd5419fa2173ec4bf
Shahariar Sohan <Sohanshahariar4@gmail.com>
2023-10-17 20:37:19 +0600

    I have added the logic of delete operation

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
 const app = express()
@@ -40,6 +40,14 @@ async function run() {
             console.log(coffee)
             const result = await coffeeCollection.insertOne(coffee);
             res.send(result)
+        })
+
+        app.delete("/coffees/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await coffeeCollection.deleteOne(query);
+            res.send(result)
+
         })
 
         await client.db("admin").command({ ping: 1 });
